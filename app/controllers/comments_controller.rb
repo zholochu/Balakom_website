d9ed7411e5e70f812e0c797fef1743cde2d49a6a
Arstanbek Zholochu <arstanbek.zholochu@gmail.com>
2023-08-10 21:55:37 -0400

    Work in progress

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,9 +5,15 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article)
   end
 
-  private
-
-  def comment_params
-    params.require(:comment).permit(:commenter, :body)
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article), status: :see_other
   end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body, :status)
+    end
 end
